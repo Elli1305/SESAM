742d2c2790dbc3856e189163c62b6027ad5d1482
Malte <malte.boettcher@uni-bielefeld.de>
2023-04-06 13:23:35 +0200

    remove unnecessary checkstyle rules

--- src/main/java/com/gpse/sesam/SesamApplication.java
+++ src/main/java/com/gpse/sesam/SesamApplication.java
@@ -6,6 +6,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SesamApplication {
 
+	private SesamApplication() {
+
+	}
+
 	public static void main(String[] args) {
 		SpringApplication.run(SesamApplication.class, args);
 	}
