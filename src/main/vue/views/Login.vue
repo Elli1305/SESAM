53f9836b106e87c68c2fff328a563e55d5e494ee
Simon Ischen <sischen@techfak.uni-bielefeld.de>
2023-04-12 11:14:40 +0200

    refs #59724 Add Login-Headline, Button for Login, and Links for registration and resetting password (Pairprogramming with Malte) @1.0h

--- src/main/vue/views/Login.vue
+++ src/main/vue/views/Login.vue
@@ -1,9 +1,17 @@
 <template>
   <q-page>
-      <div style="padding: 1em; display: flex; justify-content: center">
+      <div style="padding-top: 1em; display: flex; justify-content: center">
           <div class="q-gutter-y-md column" style="max-width: 40em; min-width: 20em">
+              <h1 style="font-size: 3em; text-align: center; margin-bottom: -0.5em">Login</h1>
               <q-input outlined v-model="eMail" label="E-Mail" :dense="dense" />
               <q-input outlined v-model="password" label="Passwort" :dense="dense" />
+              <p style="font-size: 1em">Passwort vergessen:
+                  <a href="">Passwort zurücksetzen</a>
+              </p>
+              <q-btn color="primary" label="Login" />
+              <p style="font-size: 1em">Nicht registriert:
+                  <a href="">Zur Registrierung</a>
+              </p>
           </div>
       </div>
   </q-page>
