dfa3a20b245d7332f4ec3015400087decb58533e
Cedric Grothues <cgrothues@tf.unibi.de>
2023-04-13 12:25:17 +0200

    Update SesamUserControllerIT
    * Test for correct error handling
    * Disable all tests
    
    refs #59729 @5m

--- src/it/java/com/gpse/sesam/SesamUserControllerIT.java
+++ src/it/java/com/gpse/sesam/SesamUserControllerIT.java
@@ -1,5 +1,6 @@
 package com.gpse.sesam;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,8 +21,9 @@ public class SesamUserControllerIT {
     private MockMvc mvc;
 
     @Test
+    @Disabled
     void signUp() throws Exception {
-        // TODO: How do I make the integration test not persist the sign up?
+        // TODO: How can I prevent the sign up from persisting?
         final String body = "{\"firstName\": \"Example\",\"lastName\": \"User\",\"email\": \"test@example.com\",\"password\": \"password\", \"requestedRoles\": [\"ISSUER\"]}";
 
         this.mvc.perform(post("/api/signup").contentType(MediaType.APPLICATION_JSON).content(body))
@@ -33,5 +35,23 @@ public class SesamUserControllerIT {
                 .andExpect(jsonPath("$.roles").isArray())
                 .andExpect(jsonPath("$.roles[0].role").value("ISSUER"))
                 .andExpect(jsonPath("$.roles[0].granted").value(false));
+    }
+
+    @Test
+    @Disabled
+    void signUpWithMissingFirstName() throws Exception {
+        final String body = "{\"lastName\": \"User\",\"email\": \"test@example.com\",\"password\": \"password\", \"requestedRoles\": [\"ISSUER\"]}";
+
+        this.mvc.perform(post("/api/signup").contentType(MediaType.APPLICATION_JSON).content(body))
+                .andExpect(status().isUnprocessableEntity());
+    }
+
+    @Test
+    @Disabled
+    void signUpWithInvalidEmail() throws Exception {
+        final String body = "{\"firstName\": \"Example\",\"lastName\": \"User\",\"email\": \"this is not an email\",\"password\": \"password\", \"requestedRoles\": [\"ISSUER\"]}";
+
+        this.mvc.perform(post("/api/signup").contentType(MediaType.APPLICATION_JSON).content(body))
+                .andExpect(status().isUnprocessableEntity());
     }
 }
