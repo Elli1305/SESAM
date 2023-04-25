3ff160d12a6818b5dfd05807ec2bf4a6add69c9c
Eileen Behrens <eileen.behrens@uni-bielefeld.de>
2023-04-25 17:40:45 +0200

    adds translation for German and English to App.vue and Profilansicht.vue

--- src/main/vue/main.ts
+++ src/main/vue/main.ts
@@ -17,17 +17,53 @@ import axios from "axios";
 
 const messages = {
     de: {
-        home: {header: "SESAM(Ger)"}
+        home: {
+            header: "SESAM(Ger)",
+            signup: "Registrierung",
+            signin: "Login",
+            floorplan: "Floorplan",
+            credentials: "Credentials",
+            information: "Information",
+            imprint: "Impressum",
+            logout: "Logout"
+        },
+        profile: {
+            title: "Profilansicht",
+            firstname: "Vorname",
+            lastname: "Name",
+            email: "E-Mail",
+            admin: "Admin",
+            editor: "Bearbeiter",
+            issuer: "Herausgeber"
+        }
     },
     en: {
-        home: {header: "SESAM(En)"}
+        home: {
+            header: "SESAM(En)",
+            signup: "Sign-up",
+            signin: "Sign-in",
+            floorplan: "Floorplan",
+            credentials: "Credentials",
+            information: "Information",
+            imprint: "Imprint",
+            logout: "Logout"
+        },
+        profile: {
+            title: "Profile View",
+            firstname: "First name",
+            lastname: "Name",
+            email: "E-mail",
+            admin: "Admin",
+            editor: "Editor",
+            issuer: "Issuer"
+        }
     }
 }
 
 
 const i18n = createI18n({
     legacy: false,
-    locale: 'de',
+    locale: 'en',
     allowComposition: true,
     fallbackLocale: 'en',
     messages
--- src/main/vue/views/Profilansicht.vue
+++ src/main/vue/views/Profilansicht.vue
@@ -1,23 +1,24 @@
 <template>
   <q-page class="column justify-evenly" style="padding: 0 5em" >
-    <p class="row text-h3 justify-center">Profilansicht</p>
+    <p class="row text-h3 justify-center">{{t('profile.title')}}</p>
     <div class="self-center" style="margin: 2em; width: 25em" >
-      <q-input id="prename" v-model="firstname" label="Vorname" outlined disable/>
+      <q-input id="prename" v-model="firstname" :label="t('profile.firstname')" outlined disable/>
       <q-space style="height: 1em" />
-      <q-input id="surname" v-model="lastname" label="Name" outlined disable/>
+      <q-input id="surname" v-model="lastname" :label="t('profile.lastname')" outlined disable/>
       <q-space style="height: 1em" />
-      <q-input id="email" v-model="email" label="E-Mail" outlined disable/>
+      <q-input id="email" v-model="email" :label="t('profile.email')" outlined disable/>
     </div>
     <div class="row justify-center" >
-      <q-badge ref="admin" class="row justify-center" rounded color="secondary" text-color="primary" :style="{marginRight: '2em', height: '2em', width: '10em', opacity: this.$data.adminopacity}" ><q-icon name="person" left /> Admin </q-badge>
-      <q-badge id="editor-id" class="row justify-center" rounded color="secondary" text-color="primary" :style="{marginRight: '2em', height: '2em', width: '10em', opacity: this.$data.editorOpacity}"  ><q-icon name="person" left /> Bearbeiter </q-badge>
-      <q-badge id="issuer" class="row justify-center" rounded color="secondary" text-color="primary" :style="{height: '2em', width: '10em', opacity: this.$data.issueropacity}" ><q-icon name="person" left /> Herausgeber </q-badge>
+      <q-badge class="row justify-center" rounded color="secondary" text-color="primary" :style="{marginRight: '2em', height: '2em', width: '10em', opacity: this.$data.adminopacity}" ><q-icon name="person" left />{{t('profile.admin')}}</q-badge>
+      <q-badge class="row justify-center" rounded color="secondary" text-color="primary" :style="{marginRight: '2em', height: '2em', width: '10em', opacity: this.$data.editorOpacity}"  ><q-icon name="person" left />{{t('profile.editor')}}</q-badge>
+      <q-badge class="row justify-center" rounded color="secondary" text-color="primary" :style="{height: '2em', width: '10em', opacity: this.$data.issueropacity}" ><q-icon name="person" left />{{t('profile.issuer')}}</q-badge>
     </div>
   </q-page>
 </template>
 
 <script>
 import {useUserStore} from "@/main/vue/stores/users"
+import {useI18n} from "vue-i18n";
 
 export default {
   name: "Profilansicht",
@@ -30,6 +31,7 @@ export default {
     const lastName = userStore.lastName
     const eMail = userStore.eMail
     const roles = userStore.roles
+    const { t } = useI18n()
 
     let adminOpacity = roles.includes('ADMINISTRATOR') ? "100%" : "50%"
     let editorOpacity = roles.includes('EDITOR') ? "100%" : "50%"
@@ -41,7 +43,8 @@ export default {
       email: eMail,
       adminopacity: adminOpacity,
       editorOpacity: editorOpacity,
-      issueropacity: issuerOpacity
+      issueropacity: issuerOpacity,
+      t
     }
   }
 }
