993a7a0cc5f3d4dbde5a64a0b98294242e19ab12
Eileen Behrens <eileen.behrens@uni-bielefeld.de>
2023-04-25 17:12:42 +0200

    refs #59988 fills inputs with data and changes role opacity accordingly (access to roles currently limited)

--- src/main/vue/views/Profilansicht.vue
+++ src/main/vue/views/Profilansicht.vue
@@ -1,24 +1,49 @@
 <template>
-  <q-page class="column justify-around" style="padding: 0 5em" >
-    <p class="row text-h3 justify-center" >Profilansicht</p>
-    <div class="" style="margin: 2em; width: 25em" >
-      <q-input id="prename" outlined v-model="prename" label="Vorname" disable />
+  <q-page class="column justify-evenly" style="padding: 0 5em" >
+    <p class="row text-h3 justify-center">Profilansicht</p>
+    <div class="self-center" style="margin: 2em; width: 25em" >
+      <q-input id="prename" v-model="firstname" label="Vorname" outlined disable/>
       <q-space style="height: 1em" />
-      <q-input id="surname" outlined v-model="lastname" label="Name" disable />
+      <q-input id="surname" v-model="lastname" label="Name" outlined disable/>
       <q-space style="height: 1em" />
-      <q-input id="email" outlined v-model="email" type="email" label="E-Mail" disable />
+      <q-input id="email" v-model="email" label="E-Mail" outlined disable/>
     </div>
     <div class="row justify-center" >
-      <q-badge id="issuer" class="row justify-center" rounded color="secondary" text-color="primary" style="margin-right: 2em;  width: 10em" ><q-icon name="person" left /> Herausgeber </q-badge>
-      <q-badge id="editor" class="row justify-center" rounded color="secondary" text-color="primary" style="margin-right: 2em; width: 10em"  ><q-icon name="person" left /> Bearbeiter </q-badge>
-      <q-badge id="admin" class="row justify-center" rounded color="secondary" text-color="primary" style="height: 2em; width: 10em; opacity: 50%" ><q-icon name="person" left /> Admin </q-badge>
+      <q-badge ref="admin" class="row justify-center" rounded color="secondary" text-color="primary" :style="{marginRight: '2em', height: '2em', width: '10em', opacity: this.$data.adminopacity}" ><q-icon name="person" left /> Admin </q-badge>
+      <q-badge id="editor-id" class="row justify-center" rounded color="secondary" text-color="primary" :style="{marginRight: '2em', height: '2em', width: '10em', opacity: this.$data.editorOpacity}"  ><q-icon name="person" left /> Bearbeiter </q-badge>
+      <q-badge id="issuer" class="row justify-center" rounded color="secondary" text-color="primary" :style="{height: '2em', width: '10em', opacity: this.$data.issueropacity}" ><q-icon name="person" left /> Herausgeber </q-badge>
     </div>
   </q-page>
 </template>
 
 <script>
+import {useUserStore} from "@/main/vue/stores/users"
+
 export default {
-  name: "Profilansicht"
+  name: "Profilansicht",
+  props() {
+    this.$refs.admin.$props.color = "info"
+  },
+  data() {
+    const userStore = useUserStore()
+    const firstName = userStore.firstName
+    const lastName = userStore.lastName
+    const eMail = userStore.eMail
+    const roles = userStore.roles
+
+    let adminOpacity = roles.includes('ADMINISTRATOR') ? "100%" : "50%"
+    let editorOpacity = roles.includes('EDITOR') ? "100%" : "50%"
+    let issuerOpacity = roles.includes('ISSUER') ? "100%" : "50%"
+
+    return {
+      firstname: firstName,
+      lastname: lastName,
+      email: eMail,
+      adminopacity: adminOpacity,
+      editorOpacity: editorOpacity,
+      issueropacity: issuerOpacity
+    }
+  }
 }
 </script>
 
