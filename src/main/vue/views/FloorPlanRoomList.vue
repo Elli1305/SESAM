2d25c5ca13e6cee0bb9ba5762e377cb0f9b17288
Simon Ischen <sischen@techfak.uni-bielefeld.de>
2023-05-10 22:40:46 +0200

    refs #60302 @5h: floorPlan.ts has new field for the selected rooms, Added the rooms as a list and gave the elements the functionality to be (de-)selected

--- src/main/vue/views/FloorPlanRoomList.vue
+++ src/main/vue/views/FloorPlanRoomList.vue
@@ -2,16 +2,81 @@
 <q-page-container class="no-padding no-margin">
     <q-page style="padding-right: 1em; padding-top: 2em">
         <q-list>
-            <!--<q-expansion-item v-for="" :key=""></q-expansion-item>-->
+          <q-item  v-for="room in floorPlanStore.rooms" style="padding-left: 0">
+            <q-checkbox v-model="selectedRooms" :val="room" color="blue"></q-checkbox>
+            <q-btn-dropdown
+                split
+                style="min-width: 16em"
+                :label="room.name"
+                color="primary"
+                @click="toggleRoomCheckbox(room)"
+            >
+              <div class="col no-wrap">
+                <div class="row" style="padding: 0.5em">
+                  <q-list>
+                    <q-item-label>Raumbezeichnung: {{room.name}}</q-item-label>
+                    <q-item-label>Raumtyp: Meetingraum</q-item-label>
+                    <q-item-label>Credentials: 1,2,3,4</q-item-label>
+                  </q-list>
+                </div>
+                <div v-if="userStore.authenticated">
+                  <q-separator spaced></q-separator>
+                  <div class="row justify-end" style="padding: 0.5em">
+                    <q-btn size="sm" label="Bearbeiten" color="blue"></q-btn>
+                  </div>
+                </div>
+              </div>
+            </q-btn-dropdown>
+          </q-item>
         </q-list>
-        <p>Hallo ich teste hier was und noch mehr</p>
     </q-page>
 </q-page-container>
 </template>
 
 <script>
+import {useFloorPlanStore} from "@/main/vue/stores/floorPlan";
+import {useUserStore} from "@/main/vue/stores/users";
+
 export default {
-    name: "FloorPlanRoomList"
+  name: "FloorPlanRoomList",
+
+  setup() {
+    const floorPlanStore = useFloorPlanStore()
+    const selectedRooms = floorPlanStore.selectedRooms
+    const userStore = useUserStore()
+
+    function addAllRooms() {
+        floorPlanStore.rooms.forEach((element) => {
+            selectedRooms.push(element)
+        })
+    }
+
+    function deleteAllRooms() {
+      floorPlanStore.selectedRooms.length = 0
+    }
+
+    function addRoom(element) {
+      selectedRooms.push(element)
+    }
+
+    function deleteRoom(element) {
+      selectedRooms.forEach( (item, index) => {
+        if(item === element) selectedRooms.splice(index,1);
+      });
+    }
+
+    function toggleRoomCheckbox(element) {
+      console.log(selectedRooms.includes(element))
+      if(selectedRooms.includes(element)) {
+        deleteRoom(element)
+      } else if(!selectedRooms.includes(element)) {
+        addRoom(element)
+      }
+    }
+
+    return{floorPlanStore, selectedRooms, toggleRoomCheckbox, userStore}
+  }
+
 }
 </script>
 
