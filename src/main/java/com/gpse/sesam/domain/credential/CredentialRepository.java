e5e4d20dd1fe8a507ebc518cdacaff3c07a32faa
Louisa Habich <lhabich@techfak.uni-bielefeld.de>
2023-05-24 23:00:15 +0200

    refs #60217 @0.5h corrected CS warning

--- src/main/java/com/gpse/sesam/domain/credential/CredentialRepository.java
+++ src/main/java/com/gpse/sesam/domain/credential/CredentialRepository.java
@@ -8,9 +8,9 @@ import java.util.List;
 
 public interface CredentialRepository extends CrudRepository<Credential, Long> {
     @Query(value = "SELECT cmd from CredentialCmd cmd join Credential c"
-            + " join c.category cat join cat.externalCredentials ex join c.issuer i join c.doors d join d.room r join " +
-            "r.floor f join f.building b join b.location l " +
-            "WHERE l.id = ?1 and cmd.credentialName = c.name and cmd.categoryName = cat.name")
+            + " join c.category cat join cat.externalCredentials ex join c.issuer i join c.doors d "
+            + "join d.room r join r.floor f join f.building b join b.location l "
+            + "WHERE l.id = ?1 and cmd.credentialName = c.name and cmd.categoryName = cat.name")
     List<CredentialCmd> findByLocation(Long id);
 }
 
