5e3ae11e8cac5cc946e90039178d6f9e6ec99cc0
Louisa Habich <lhabich@techfak.uni-bielefeld.de>
2023-06-03 12:54:56 +0200

    refs #60573 @0.25h fixed cs errors

--- src/main/java/com/gpse/sesam/domain/credential/CredentialService.java
+++ src/main/java/com/gpse/sesam/domain/credential/CredentialService.java
@@ -22,6 +22,6 @@ public interface CredentialService {
 
 	List<Credential> credentialFindByLocation(Long id);
 
-	List<CredentialCmd> getCredentialByLocation(final Long id);
+	List<CredentialCmd> getCredentialByLocation(Long id);
 
 }
