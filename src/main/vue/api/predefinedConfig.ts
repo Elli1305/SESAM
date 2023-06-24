c76a34fb97e30aa574f781c0f2e0fbfdf85a3dfd
Simon Ischen <sischen@techfak.uni-bielefeld.de>
2023-06-24 14:07:32 +0200

    refs #61072 @1h: Added functions to config-store, added api-calls

--- src/main/vue/api/predefinedConfig.ts
+++ src/main/vue/api/predefinedConfig.ts
@@ -6,5 +6,22 @@ export default {
 
     getAllConfigs(): Promise<AxiosResponse<PredefinedConfiguration[]>> {
         return axios.get("api/preConfig/allConfigs")
+    },
+
+    getConfig(id: BigInt): Promise<AxiosResponse<PredefinedConfiguration>> {
+        return axios.get(`api/preConfig/${id}`)
+    },
+
+    saveConfig(config: PredefinedConfiguration): Promise<AxiosResponse<void>> {
+        return axios.post("api/preConfig/create", config)
+    },
+
+    updateConfig(config: PredefinedConfiguration): Promise<AxiosResponse<void>> {
+        return axios.post("api/preConfig/update", config)
+    },
+
+    deleteConfig(id: BigInt): Promise<AxiosResponse<void>> {
+        return axios.delete(`api/preConfig/delete/${id}`)
     }
+
 }
