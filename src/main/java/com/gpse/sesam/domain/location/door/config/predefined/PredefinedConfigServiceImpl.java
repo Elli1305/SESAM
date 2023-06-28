50eb87f87c5c6ec178016f18d7f19dc339b45849
Simon Ischen <sischen@techfak.uni-bielefeld.de>
2023-06-28 23:29:19 +0200

    refs #61071 @2h: Created Mapper to convert PredefinedConfig into Cmd

--- src/main/java/com/gpse/sesam/domain/location/door/config/predefined/PredefinedConfigServiceImpl.java
+++ src/main/java/com/gpse/sesam/domain/location/door/config/predefined/PredefinedConfigServiceImpl.java
@@ -1,11 +1,15 @@
 package com.gpse.sesam.domain.location.door.config.predefined;
 
+import com.gpse.sesam.domain.credential.credentials.CredentialService;
 import com.gpse.sesam.domain.location.door.config.ProofConfig;
 import com.gpse.sesam.util.ConfigCmdMapper;
+import com.gpse.sesam.web.cmd.DoorConfigCmd;
 import com.gpse.sesam.web.cmd.PredefinedConfigCmd;
+import com.gpse.sesam.web.exception.PredefinedConfigNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.text.ParseException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
@@ -15,9 +19,16 @@ public class PredefinedConfigServiceImpl implements PredefinedConfigService {
 
     private final PredefinedConfigRepository predefinedConfigRepository;
 
+    private final CredentialService credentialService;
+
+    private final ConfigCmdMapper configCmdMapper;
+
     @Autowired
-    public PredefinedConfigServiceImpl(final PredefinedConfigRepository predefinedConfigRepository) {
+    public PredefinedConfigServiceImpl(final PredefinedConfigRepository predefinedConfigRepository,
+                                       CredentialService credentialService) {
         this.predefinedConfigRepository = predefinedConfigRepository;
+        this.credentialService = credentialService;
+        this.configCmdMapper = new ConfigCmdMapper(credentialService);
     }
 
     @Override
@@ -30,13 +41,23 @@ public class PredefinedConfigServiceImpl implements PredefinedConfigService {
     }
 
     @Override
-    public Optional<PredefinedConfig> getPreConfig(final Long id) {
-        return predefinedConfigRepository.findById(id);
+    public PredefinedConfigCmd getPreConfig(final Long id) {
+        PredefinedConfig predefinedConfig = predefinedConfigRepository.findById(id).orElseThrow(()
+                -> new PredefinedConfigNotFoundException("PredefinedConfig with id: " + id + " not found"));
+        try {
+            DoorConfigCmd in = configCmdMapper.toCmd(predefinedConfig.getDoorIn());
+            DoorConfigCmd out = configCmdMapper.toCmd(predefinedConfig.getDoorOut());
+            PredefinedConfigCmd predefinedConfigCmd = new PredefinedConfigCmd(predefinedConfig.getId(),
+                    predefinedConfig.getName(), in, out);
+            return predefinedConfigCmd;
+        } catch (ParseException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
     public void update(PredefinedConfigCmd predefinedConfig) {
-        final Optional<PredefinedConfig> tempPreConfig = getPreConfig(predefinedConfig.getId());
+        final Optional<PredefinedConfig> tempPreConfig = predefinedConfigRepository.findById(predefinedConfig.getId());
         final ProofConfig configIn = ConfigCmdMapper.fromCmd(predefinedConfig.getDoorConfigIn());
         final ProofConfig configOut = ConfigCmdMapper.fromCmd(predefinedConfig.getDoorConfigOut());
         if (tempPreConfig.isPresent()) {
