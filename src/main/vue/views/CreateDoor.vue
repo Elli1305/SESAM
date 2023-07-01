ceb4bb13a23bff67510059da35a741effcfbdc09
Simon Ischen <sischen@techfak.uni-bielefeld.de>
2023-07-01 16:00:21 +0200

    refs #61072 @1h: PredefinedConfigs can be loaded when creating/editing a door

--- src/main/vue/views/CreateDoor.vue
+++ src/main/vue/views/CreateDoor.vue
@@ -1,6 +1,6 @@
 <template>
   <q-dialog ref="dialog" @hide="onDialogHide">
-    <q-card style="min-width: 45em">
+    <q-card style="min-width: 60em">
       <q-card-section>
         <div class="text-h6">Tür zuweisen</div>
       </q-card-section>
@@ -28,6 +28,29 @@
             </q-item>
           </template>
         </q-select>
+        <q-select
+             class="q-ml-md"
+             style="min-width: 20em"
+             filled
+             use-input
+             hide-selected
+             fill-input
+             input-debounce="0"
+             @filter="filterFn"
+             label="Konfiguration auswählen"
+             option-label="name"
+             v-model="selectedConfig"
+             :options="configOptions"
+             clearable
+          >
+            <template v-slot:no-option>
+                <q-item>
+                    <q-item-section class="text-grey">
+                        No results
+                    </q-item-section>
+                </q-item>
+            </template>
+        </q-select>
       </q-card-section>
       <door-config ref="configIn" :door-config="doorConfigIn"
                    :direction="JSON.stringify(this.doorConfigIn) !== JSON.stringify(this.doorConfigOut) ? Direction.IN : Direction.BOTH"
@@ -44,9 +67,11 @@
 </template>
 
 <script>
-import {ref} from "vue";
+import {ref, watch} from "vue";
 import DoorConfig from "@/main/vue/views/DoorConfig.vue";
 import {Direction} from "@/main/vue/entity/doorConfiguration";
+import {useConfigStore} from "@/main/vue/stores/config";
+import {storeToRefs} from "pinia";
 
 export default {
   computed: {
@@ -122,9 +147,43 @@ export default {
     },
   },
   setup(props) {
-    const room = ref(null)
-    const doorName = ref('')
-    const roomOptions = ref(props.rooms)
+      const room = ref(null)
+      const doorName = ref('')
+      const roomOptions = ref(props.rooms)
+      const configStore = useConfigStore()
+      configStore.getAllConfigs()
+      const {allPreConfigs} = storeToRefs(configStore)
+      const configOptions = ref()
+      configOptions.value = configStore.allPreConfigs
+      const selectedConfig = ref()
+
+      const configIn = ref()
+      const configOut = ref()
+
+      watch(allPreConfigs, () => {
+          configOptions.value = configStore.allPreConfigs
+      })
+
+      watch(selectedConfig, async () => {
+          if (selectedConfig.value == null) {
+              configIn.value.configDescription = null
+              configIn.value.qSelects.configParts = []
+              configOut.value.configDescription = null
+              configOut.value.qSelects.configParts = []
+              configIn.value.direction = Direction.BOTH
+          } else {
+              await configStore.getConfig(selectedConfig.value.id)
+              let chosenConfig = configStore.currentConfig
+              configIn.value.configDescription = chosenConfig?.doorConfigIn.description
+              configIn.value.qSelects.configParts = chosenConfig?.doorConfigIn.configParts
+              configOut.value.configDescription = chosenConfig?.doorConfigOut.description
+              configOut.value.qSelects.configParts = chosenConfig?.doorConfigOut.configParts
+              if (JSON.stringify(chosenConfig?.doorConfigIn) !== JSON.stringify(chosenConfig?.doorConfigOut)) {
+                  configIn.value.direction = Direction.IN
+                  configOut.value.direction = Direction.OUT
+              }
+          }
+      })
 
     if (props.door) {
       doorName.value = props.door.name
@@ -140,7 +199,11 @@ export default {
     return {
       room,
       filterFn,
-      doorName
+      doorName,
+        configOptions,
+        selectedConfig,
+        configOut,
+        configIn
     }
   }
 }
