bae8cfa973dab1c1180d531c20baf09fba3c81f5
Cedric Grothues <cgrothues@tf.unibi.de>
2023-07-10 22:47:39 +0200

    Fix CredentialAdministration search

--- src/main/vue/views/CredentialAdministration.vue
+++ src/main/vue/views/CredentialAdministration.vue
@@ -1,43 +1,40 @@
 <template>
   <q-page class="column justify-evenly" style="padding: 2em 5em">
-    <p class="row text-h3 justify-center">{{t('admin.credentialAdministration.title')}}</p>
+    <p class="row text-h3 justify-center">{{ t('admin.credentialAdministration.title') }}</p>
     <div class="row self-center">
-      <q-table
-          style="width: 80vw; height: 50vh"
-          :rows-per-page-options="[0]"
-          :columns="columns"
-          :rows="rows"
-          :separator="'cell'"
-          :filter="search"
-          row-key="name">
-      <template v-slot:top-left>
-        <q-select style="width: 12em" dense v-model="selectedTypes" :options="options" borderless emit-value :label="t('admin.credentialAdministration.type')" map-options multiple/>
+      <q-table :columns="columns" :filter="search" :rows="rows" :rows-per-page-options="[0]"
+               :separator="'cell'" row-key="name" style="width: 80vw; height: 50vh">
+        <template v-slot:top-left>
+          <q-select v-model="selectedTypes" :label="t('admin.credentialAdministration.type')" :options="options" borderless dense emit-value
+                    map-options multiple style="width: 12em"/>
         </template>
-      <template v-slot:top-right>
-        <q-btn rounded borderless color="grey-6" flat icon="add" :label="t('admin.credentialAdministration.new')" to="/add_credential"/>
-        <q-input v-model="filter" borderless class="q-ml-lg" debounce="300" dense :placeholder="t('common.search')">
-          <template v-slot:append>
-            <q-icon name="search"/>
-          </template>
-        </q-input>
-      </template>
-      <template v-slot:body-cell-name="props">
-        <q-td :props="props">
-          {{ props.value }}
-        </q-td>
-      </template>
-      <template v-slot:body-cell-type="props">
-        <q-td :props="props">
-          <q-chip v-if="props.value" color="primary" label="Internal" text-color="white"/>
-          <q-chip v-else color="secondary" label="External" text-color="white"/>
-        </q-td>
-      </template>
-      <template v-slot:body-cell-actions="props">
-        <q-td :props="props">
-          <q-btn :to="`/credential_administration/${'issuer' in props.row ? 'internal' : 'external'}/${props.row.id}`" dense round flat color="grey" icon="edit"></q-btn>
+        <template v-slot:top-right>
+          <q-btn :label="t('admin.credentialAdministration.new')" borderless color="grey-6" flat icon="add" rounded
+                 to="/add_credential"/>
+          <q-input v-model="search" :placeholder="t('common.search')" borderless class="q-ml-lg" debounce="300" dense>
+            <template v-slot:append>
+              <q-icon name="search"/>
+            </template>
+          </q-input>
+        </template>
+        <template v-slot:body-cell-name="props">
+          <q-td :props="props">
+            {{ props.value }}
           </q-td>
-      </template>
-    </q-table>
+        </template>
+        <template v-slot:body-cell-type="props">
+          <q-td :props="props">
+            <q-chip v-if="props.value" color="primary" label="Internal" text-color="white"/>
+            <q-chip v-else color="secondary" label="External" text-color="white"/>
+          </q-td>
+        </template>
+        <template v-slot:body-cell-actions="props">
+          <q-td :props="props">
+            <q-btn :to="`/credential_administration/${'issuer' in props.row ? 'internal' : 'external'}/${props.row.id}`"
+                   color="grey" dense flat icon="edit" round></q-btn>
+          </q-td>
+        </template>
+      </q-table>
     </div>
   </q-page>
 </template>
@@ -46,11 +43,11 @@
 import {QSelectOption, QTableColumn} from "quasar";
 import {useCredentialsStore} from "@/main/vue/stores/credential";
 import {computed, Ref, ref} from "vue";
-import {Credential, ExternalCredential} from "@/main/vue/entity/credentialDefinition";
+import {ExternalCredential, InternalCredential} from "@/main/vue/entity/credentialDefinition";
 import {useI18n} from "vue-i18n";
 
 const {t} = useI18n();
-const columns: QTableColumn<Credential | ExternalCredential>[] = [
+const columns: QTableColumn<InternalCredential | ExternalCredential>[] = [
   {
     name: 'name',
     required: true,
