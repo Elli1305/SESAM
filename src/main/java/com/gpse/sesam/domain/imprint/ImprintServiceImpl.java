0727500b3cef2945fd6a6ea7b8879b894cb06794
Aya Bezine <abezine@techfak.uni-bielefeld.de>
2023-07-10 17:14:05 +0200

    refs #61430 @1h adding javadoc

--- src/main/java/com/gpse/sesam/domain/imprint/ImprintServiceImpl.java
+++ src/main/java/com/gpse/sesam/domain/imprint/ImprintServiceImpl.java
@@ -1,27 +1,42 @@
 package com.gpse.sesam.domain.imprint;
 
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import java.time.LocalDateTime;
 import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.StreamSupport;
 
+/**
+ * The ImprintServiceImpl class implements the {@link ImprintService} interface.
+ * It provides methods for managing the Imprint entries in the database.
+ */
 @Service
 public class ImprintServiceImpl implements ImprintService {
 
+	/**
+	 * The repository for the Imprint entity.
+	 */
 	@Autowired
 	private ImprintRepository imprintRepository;
 
+	/**
+	 * Creates a new Imprint entry with the provided content.
+	 *
+	 * @param content the content of the new Imprint entry
+	 */
 	@Override
 	public void createImprintEntry(final String content) {
 		final Imprint newEntry = new Imprint(content, LocalDateTime.now());
 		imprintRepository.save(newEntry);
 	}
 
+	/**
+	 * Retrieves the latest Imprint entry.
+	 *
+	 * @return the content of the latest Imprint entry
+	 */
 	@Override
 	public String getLatestImprintEntry() {
 		final List<Imprint> imprintEntries = StreamSupport
@@ -34,6 +49,9 @@ public class ImprintServiceImpl implements ImprintService {
 				.orElse(null);
 	}
 
+	/**
+	 * Deletes all Imprint entries in the database.
+	 */
 	@Override
 	public void deleteImprintEntry() {
 		imprintRepository.deleteAll();
